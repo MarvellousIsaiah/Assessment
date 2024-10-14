4777e647e918de3cc04b272991ccb8fe23bd0f20
MarvellousIsaiah <isaiahugoeze@gmail.com>
2024-10-14 15:06:56 +0100

    update

--- src/Array.java
+++ src/Array.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Arrary {
+import java.util.Arrays;
+
+public class Array {
+
+    public static void main(String[] args) {
+        int[] arr = {500, 200, 1, 4, 5, 90, 100, 30000, 23, 45, 67, 29, 12, 10, 2, 14};
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("The minimum value in the array is: " + min);
+        System.out.println("The maximum value in the array is: " + max);
+    }
 }
+
+// reverseArray(arr);
+          //  System.out.print(" "+number[i]);
+      //  }
+        //}
+   // public static void main(String[]arg){
+      //  reverseArray(arr);
+       // System.out.println(Arrays.toString(arr));
+
+
+
+
+
+
